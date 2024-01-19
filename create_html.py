87d48a3c9b8b03ce330d397bad2d916dfcc9de21
Björn Leví Gunnarsson <bjornlevi@gmail.com>
2024-01-19 15:53:40 +0000

    0.9 update with better navigation and styles

---
+++ create_html.py
@@ -0,0 +1,173 @@
+from jinja2 import Template, Environment, FileSystemLoader
+import sqlite3, os
+
+conn = sqlite3.connect('thing.db')
+cursor = conn.cursor()
+
+# Búa til html um þingskjöl
+query = """WITH RankedFlutningsmadur AS (
+    SELECT
+        ts.*,
+        f.nafn AS flutningsmadur_name,
+        f.radherra AS flutningsmadur_radherra,
+        ROW_NUMBER() OVER (PARTITION BY ts.malsnumer ORDER BY f.rada) AS row_num
+    FROM
+        thingskjal ts
+    JOIN
+        flutningsmadur f ON ts.skjalsnumer = f.skjalsnumer
+)
+SELECT
+    r.malsnumer,
+    tm.malsheiti,
+    tm.stadamals,
+    r.utbyting,
+    r.skjalategund,
+    tm.html,
+    r.flutningsmadur_name,
+    r.flutningsmadur_radherra,
+    tm.thingflokkur
+FROM
+    RankedFlutningsmadur r
+JOIN
+    thingmal tm ON r.malsnumer = tm.malnumer
+LEFT JOIN
+    thingmenn tm ON r.flutningsmadur_name = tm.nafn
+WHERE
+    r.skjalategund IN ('frumvarp', 'þáltill.', 'stjórnarfrumvarp', 'stjórnartillaga')
+    AND r.row_num = 1;"""
+
+#Prepare navigation
+# Directory containing HTML files
+html_directory = "./"
+
+# List all HTML files in the directory
+html_files = [f for f in os.listdir(html_directory) if f.endswith(".html")]
+
+# Filter out files containing "template" in their name
+filtered_files = [f for f in html_files if "template" not in f.lower()]
+
+aliases = {
+    "fyrirspurnir_svarad.html": "Svaraðar fyrirspurnir",
+    "fyrirspurnir_osvarad.html": "Ósvaraðar fyrirspurnir",
+    "thingmal_rikisstjorn_bida_umraedu.html": "Bíða umræðu",
+    "thingmal_rikisstjorn_i_nefnd.html": "Í nefnd",
+    "thingmal_rikisstjorn_samthykkt.html": "Samþykkt",
+    "thingmal_thingmannamal_bida_umraedu.html": "Bíða umræðu",
+    "thingmal_thingmannamal_i_nefnd.html": "Í nefnd",
+    "thingmal_thingmannamal_samthykkt.html": "Samþykkt",
+    # Add more entries as needed
+}
+
+# Group files by categories
+categories = {
+    "Ríkisstjórnarmál": [f for f in filtered_files if "thingmal_rikisstjorn" in f.lower()],
+    "Þingmannamál": [f for f in filtered_files if "thingmal_thingmannamal" in f.lower()],
+    "Fyrirspurnir": [f for f in filtered_files if "fyrirspurnir" in f.lower()],
+}
+
+
+cursor.execute(query)
+results = cursor.fetchall()
+columns = [column[0] for column in cursor.description]
+# Convert the list of tuples to a list of dictionaries
+rows = [dict(zip(columns, row)) for row in results]
+
+# Specify the template file and create a Jinja2 environment
+template_file = 'thingmal_template.html'
+env = Environment(loader=FileSystemLoader('.'))  # Adjust the loader path accordingly
+
+# Load the template
+template = env.get_template(template_file)
+
+# Render the template with the query result
+output_html = template.render(rows=rows, filter='rikisstjorn_samþykkt', categories=categories, aliases=aliases)
+# Write the output to an HTML file
+with open('thingmal_rikisstjorn_samthykkt.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='rikisstjorn_i_nefnd', categories=categories, aliases=aliases)
+with open('thingmal_rikisstjorn_i_nefnd.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='rikisstjorn_bida_umraedu', categories=categories, aliases=aliases)
+with open('thingmal_rikisstjorn_bida_umraedu.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='thingmannamal_bida_umraedu', categories=categories, aliases=aliases)
+with open('thingmal_thingmannamal_bida_umraedu.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='thingmannamal_i_nefnd', categories=categories, aliases=aliases)
+with open('thingmal_thingmannamal_i_nefnd.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='thingmannamal_samthykkt', categories=categories, aliases=aliases)
+with open('thingmal_thingmannamal_samthykkt.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(rows=rows, filter='none', categories=categories, aliases=aliases)
+with open('thingmal.html', 'w') as f:
+    f.write(output_html)
+
+print("Upplýsingar um þingskjöl tilbúin")
+
+#Búa til html um fyrirspurnir
+query = """SELECT
+    t1.id,
+    t1.skjalsnumer,
+    fm.nafn,
+    t1.malsnumer,
+    tm.html,
+    tm.malsheiti,
+    t1.utbyting,
+    t1.skjalategund,
+    CASE
+        WHEN t1.skjalategund = 'svar' THEN t2.utbyting
+        ELSE strftime('%Y-%m-%d', 'now')
+    END AS svar_dagsetning,
+    CASE
+        WHEN t1.skjalategund = 'svar' THEN (julianday(t2.utbyting) - julianday(t1.utbyting)) * 5 / 7
+        ELSE (julianday(strftime('%Y-%m-%d', 'now')) - julianday(t1.utbyting)) * 5 / 7
+    END AS days_between
+FROM
+    thingskjal t1
+LEFT JOIN
+    thingskjal t2 ON t1.malsnumer = t2.malsnumer AND t2.skjalategund = 'svar'
+JOIN
+    thingmal tm ON t1.malsnumer = tm.malnumer
+JOIN
+    flutningsmadur fm ON t1.skjalsnumer = fm.skjalsnumer
+WHERE
+    t1.skjalategund = 'fsp. til skrifl. svars';"""
+
+cursor.execute(query)
+results = cursor.fetchall()
+columns = [column[0] for column in cursor.description]
+# Convert the list of tuples to a list of dictionaries
+rows = [dict(zip(columns, row)) for row in results]
+
+# Specify the template file and create a Jinja2 environment
+template_file = 'fyrirspurnir_template.html'
+template = env.get_template(template_file)
+
+# Render the template with the query results
+output_html = template.render(filter="osvarad",rows=rows, categories=categories, aliases=aliases)
+with open('fyrirspurnir_osvarad.html', 'w') as f:
+    f.write(output_html)
+
+output_html = template.render(filter="svarad", rows=rows, categories=categories, aliases=aliases)
+with open('fyrirspurnir_svarad.html', 'w') as f:
+    f.write(output_html)
+
+conn.close()
+
+#create index file
+# Jinja2 template for index.html
+template_loader = FileSystemLoader(searchpath="./")
+template_env = Environment(loader=template_loader)
+template = template_env.get_template("index_template.html")
+
+# Generate index.html
+output = template.render(categories=categories, aliases=aliases)
+with open("index.html", "w") as index_file:
+    index_file.write(output)
---
+++ get_data.py
@@ -0,0 +1,464 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import xmltodict
+import sqlite3
+import sys, os
+import json
+
+force = False
+try:
+    if sys.argv[1] == "True":
+      force = True
+except:
+    sys.exit(0)
+
+def cache_file(url, contents):
+  file_name = "cache/"+url.replace('/','-')
+  with open(file_name, "w") as f:
+     f.write(contents)
+
+def get_xml_data(url):
+  #get new files
+  response = requests.get(url)
+  #cache file
+  cache_file(url, response.text)
+  return xmltodict.parse(response.text)
+
+#functions
+def cache_or_fetch(url, force):
+  try: 
+    os.mkdir("cache")
+  except:
+    pass
+
+  file_name = "cache/"+url.replace('/','-')
+
+  if force:
+    return get_xml_data(url)
+  else:
+    if os.path.exists(file_name):
+      #file exists, return file contents
+      with open(file_name, "r") as f:
+        return xmltodict.parse(f.read())
+    else:
+      return get_xml_data(url)
+
+def get_thingflokkur(url):
+    data = cache_or_fetch(url, force)
+    if isinstance(data['þingmaður']['þingsetur']['þingseta'], list):
+        return data['þingmaður']['þingsetur']['þingseta'][-1]['þingflokkur']['#text']
+    else:
+        return data['þingmaður']['þingsetur']['þingseta']['þingflokkur']['#text']
+
+#Fjarlægja gamla gagnagrunninn
+try:
+    if os.path.exists('thing.db'):
+        os.remove('thing.db')
+except Exception as e:
+    print(e)
+    sys.exit(0)
+
+# Ná í upplýsingar um löggjafarþing
+print("Næ í upplýsingar um löggjafarþing")
+url = "https://www.althingi.is/altext/xml/loggjafarthing/yfirstandandi/"
+data = cache_or_fetch(url, force)
+
+# Extract information from the parsed data
+yfirstandandi = data['löggjafarþing']['þing']
+
+try:
+    current_session = yfirstandandi[u'@númer']
+except:
+    print(u'Tókst ekki að ná í núverandi þing, reyndu aftur síðar. Mögulega er xml þjónusta þingsins niðri')
+    sys.exit(0)
+
+print(f"Næ í gögn um þing númer: {current_session}")
+
+# Ná í upplýsingar um þingmenn
+print("Næ í upplýsingar um þingmenn")
+url = "https://www.althingi.is/altext/xml/thingmenn/?lthing=" + current_session
+data = cache_or_fetch(url, force)
+
+# Safna saman upplýsingum um þingmenn í lista
+data_list = []
+
+# Extract information from the data
+thingmenn_list = data['þingmannalisti']['þingmaður']
+
+counter = 0
+for thingmadur in thingmenn_list:
+    counter += 1
+    if counter%20 == 0:
+        print(str(round(counter/len(thingmenn_list),2)*100)+"%", end='                \r')  
+    id_value = int(thingmadur['@id'])
+    nafn = thingmadur['nafn']
+    faedingardagur = thingmadur['fæðingardagur']
+    skammstofun = thingmadur['skammstöfun']
+
+    # Extracting URLs from the 'xml' and 'html' elements
+    thingseta = thingmadur['xml']['þingseta']
+    thingflokkur = get_thingflokkur(thingseta)
+
+    data_list.append((id_value, nafn, faedingardagur, skammstofun, thingflokkur))
+
+# Create an SQLite database and table structure
+conn = sqlite3.connect('thing.db')
+cursor = conn.cursor()
+
+# Customize the table schema based on your XML structure
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS thingmenn (
+        id INTEGER PRIMARY KEY,
+        nafn TEXT,
+        faedingardagur DATE,
+        skammstofun TEXT,
+        thingflokkur TEXT
+    )
+''')
+
+# Insert the data into the SQLite database
+cursor.executemany('''
+    INSERT INTO thingmenn (id, nafn, faedingardagur, skammstofun, thingflokkur)
+    VALUES (?, ?, ?, ?, ?)
+''', data_list)
+
+# Commit the changes and close the connection
+conn.commit()
+
+# Ná í málalista þings
+print("Næ í málaskrá þingsins")
+url = "https://www.althingi.is/altext/xml/thingmalalisti/?lthing=" + current_session
+data = cache_or_fetch(url, force)
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS malaskra (
+        id INTEGER PRIMARY KEY,
+        malsnumer INTEGER,
+        thingnumer INTEGER,
+        malsflokkur TEXT,
+        malsheiti TEXT,
+        efnisgreining TEXT,
+        malsstegund TEXT,
+        html TEXT,
+        xml TEXT,
+        UNIQUE(malsnumer, thingnumer)
+    )
+''')
+
+# Extract information from the parsed data and insert into the database
+mal_list = data['málaskrá']['mál']
+
+for mal in mal_list:
+    malsnumer = int(mal['@málsnúmer'])
+    thingnumer = int(mal['@þingnúmer'])
+    malsflokkur = mal['@málsflokkur']
+    malsheiti = mal['málsheiti']
+    efnisgreining = mal['efnisgreining'] if 'efnisgreining' in mal else None
+    malsstegund = mal['málstegund']['heiti']
+    html = mal['html']
+    xml = mal['xml']
+
+    # Use a transaction for atomicity
+    conn.execute('BEGIN')
+
+    try:
+        # Try to insert the record
+        cursor.execute('''
+            INSERT OR IGNORE INTO malaskra (malsnumer, thingnumer, malsflokkur, malsheiti, efnisgreining, malsstegund, html, xml)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (malsnumer, thingnumer, malsflokkur, malsheiti, efnisgreining, malsstegund, html, xml))
+
+        # Commit the changes
+        conn.commit()
+
+    except sqlite3.Error as e:
+        # Rollback the transaction in case of an error
+        conn.rollback()
+        print(f"Error: {e}")
+
+# Ná í upplýsingar um þingskjöl
+print("Næ i þingskjalalista")
+url = "https://www.althingi.is/altext/xml/thingskjol/?lthing=" + current_session
+data = cache_or_fetch(url, force)
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS thingskjol (
+        id INTEGER PRIMARY KEY,
+        skjalsnumer INTEGER,
+        thingnumer INTEGER,
+        utbyting TEXT,
+        skjalategund TEXT,
+        html TEXT,
+        pdf TEXT,
+        xml TEXT,
+        UNIQUE(skjalsnumer, thingnumer)
+    )
+''')
+
+# Extract information from the parsed data and insert or update in the database
+thingskjol_list = data['þingskjöl']['þingskjal']
+
+for thingskjal in thingskjol_list:
+    skjalsnumer = int(thingskjal['@skjalsnúmer'])
+    thingnumer = int(thingskjal['@þingnúmer'])
+    utbyting = thingskjal['útbýting']
+    skjalategund = thingskjal['skjalategund']
+    xml = thingskjal['slóð']['xml']
+
+    # Use a transaction for atomicity
+    conn.execute('BEGIN')
+
+    try:
+        # Try to insert the record
+        cursor.execute('''
+            INSERT OR IGNORE INTO thingskjol (skjalsnumer, thingnumer, utbyting, skjalategund, xml)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (skjalsnumer, thingnumer, utbyting, skjalategund, xml))
+
+        # Commit the changes
+        conn.commit()
+
+    except sqlite3.Error as e:
+        # Rollback the transaction in case of an error
+        conn.rollback()
+        print(f"Error: {e}")
+
+#Ná í upplýsingar um einstaka þingskjöl
+print("Næ í upplýsingar um einstaka þingskjöl")
+cursor.execute('select xml from thingskjol')
+xml_link_list = cursor.fetchall() #listi af xml tenglum fyrir öll þingskjöl
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS thingskjal (
+        id INTEGER PRIMARY KEY,
+        skjalsnumer INTEGER,
+        thingnumer INTEGER,
+        malsnumer INTEGER,
+        utbyting TEXT,
+        skjalategund TEXT,
+        html TEXT,
+        pdf TEXT,
+        UNIQUE(skjalsnumer, thingnumer)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS flutningsmadur (
+        id INTEGER PRIMARY KEY,
+        skjalsnumer INTEGER,
+        rada INTEGER,
+        nafn TEXT,
+        radherra TEXT,
+        xml TEXT
+    )
+''')
+conn.commit()
+
+counter = 0
+for xml_link in xml_link_list:    
+    counter += 1
+    data = cache_or_fetch(xml_link[0], force)
+    if counter%20 == 0:
+        print(str(round(counter/len(xml_link_list),2)*100)+"%", end='               \r')
+
+    # Extract information from the parsed data and insert or update in the database
+    thingskjal = data['þingskjal']['þingskjal']
+    skjalsnumer = int(thingskjal['@skjalsnúmer'])
+    mal = data['þingskjal']['málalisti']['mál']
+    malsnumer = int(mal['@málsnúmer'])    
+    thingnumer = int(thingskjal['@þingnúmer'])
+    utbyting = thingskjal['útbýting']
+    skjalategund = thingskjal['skjalategund']
+
+    # Insert or ignore into thingskjal table
+    cursor.execute('''
+        INSERT OR IGNORE INTO thingskjal (skjalsnumer, malsnumer, thingnumer, utbyting, skjalategund)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (skjalsnumer, malsnumer, thingnumer, utbyting, skjalategund))
+
+    # Insert or ignore into flutningsmaður table
+    try:
+        flutningsmenn = ''
+        try: 
+            flutningsmenn = thingskjal['flutningsmenn']['flutningsmaður'] #can be either list or dict
+        except:
+            flutningsmenn = thingskjal['flutningsmenn']['nefnd']['flutningsmaður'] #is a list
+        #dæmi: 
+        '''
+        [
+            {'@röð': '1', '@id': '1449', 'nafn': 'Friðjón R. Friðjónsson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1449'}, 
+            {'@röð': '2', '@id': '1176', 'nafn': 'Vilhjálmur Árnason', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1176'}, 
+            {'@röð': '3', '@id': '688', 'nafn': 'Jón Gunnarsson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=688'}, 
+            {'@röð': '4', '@id': '1039', 'nafn': 'Bryndís Haraldsdóttir', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1039'}
+        ]
+        '''
+        # þarf að laga fyrir flutning nefndar
+        #dæmi:
+        '''
+        {
+        '@skjalsnúmer': '635', '@þingnúmer': '154', '@málsflokkur': 'A', 'útbýting': '2023-11-28 15:01', 'skjalategund': 'frumvarp nefndar', 
+        'flutningsmenn': 
+            {'nefnd': 
+                {
+                '@id': '203', 'hluti': None, 'heiti': 'atvinnuveganefnd', 
+                'flutningsmaður': 
+                    [
+                        {'@röð': '1', '@id': '1345', 'nafn': 'Þórarinn Ingi Pétursson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1345'}, 
+                        {'@röð': '2', '@id': '1370', 'nafn': 'Lilja Rannveig Sigurgeirsdóttir', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1370'}, 
+                        {'@röð': '3', '@id': '1425', 'nafn': 'Gísli Rafn Ólafsson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1425'}, 
+                        {'@röð': '4', '@id': '1157', 'nafn': 'Ásmundur Friðriksson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1157'}, 
+                        {'@röð': '5', '@id': '1012', 'nafn': 'Bjarkey Olsen Gunnarsdóttir', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1012'}, 
+                        {'@röð': '6', '@id': '1276', 'nafn': 'Hanna Katrín Friðriksson', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1276'}, 
+                        {'@röð': '7', '@id': '999', 'nafn': 'Óli Björn Kárason', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=999'}, 
+                        {'@röð': '8', '@id': '1332', 'nafn': 'Inga Sæland', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=1332'}
+                    ]
+                }
+            }, 
+        'slóð': {'html': 'http://www.althingi.is/altext/154/s/0635.html', 'pdf': 'http://www.althingi.is/altext/pdf/154/s/0635.pdf'}}
+        '''
+        # þarf að taka tillit til þess ef ráðherra er flutningsmaður
+        #dæmi: 
+        #{'@röð': '1', '@id': '730', 'ráðherra': 'matvælaráðherra', 'nafn': 'Svandís Svavarsdóttir', 'xml': 'http://www.althingi.is/altext/xml/thingmenn/thingmadur/?nr=730'}
+
+        if isinstance(flutningsmenn, list):
+            for flutningsmadur in flutningsmenn:
+                rada = int(flutningsmadur['@röð'])
+                nafn = flutningsmadur['nafn']
+                xml = flutningsmadur['xml']
+                cursor.execute('''
+                    INSERT OR IGNORE INTO flutningsmadur (skjalsnumer, rada, nafn, xml)
+                    VALUES (?, ?, ?, ?)
+                ''', (skjalsnumer, rada, nafn, xml))
+        elif isinstance(flutningsmenn, dict):
+            rada = int(flutningsmenn['@röð'])
+            nafn = ''
+            try:
+                radherra = flutningsmenn['ráðherra']
+            except:
+                pass
+            nafn = flutningsmenn['nafn']
+            xml = flutningsmenn['xml']
+            cursor.execute('''
+                INSERT OR IGNORE INTO flutningsmadur (skjalsnumer, rada, nafn, radherra, xml)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (skjalsnumer, rada, nafn, radherra, xml))
+    except:
+        #þetta geta verið þingskjöl á við nefndarálit, lög í heild, etc. Sleppa
+        pass
+    # Commit the changes
+    conn.commit()
+
+# Ná í upplýsingar um einstaka mál
+print("Næ í upplýsingar um einstaka mál")
+
+# Create table if not exists
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS thingmal (
+        malnumer INTEGER PRIMARY KEY,
+        thingnumer INTEGER,
+        malsheiti TEXT,
+        malstegund TEXT,
+        stadamals TEXT,
+        framsogumadur TEXT,
+        html TEXT,
+        xml TEXT,
+        UNIQUE(malnumer, thingnumer)
+    )
+''')
+
+cursor.execute('select xml from malaskra')
+xml_link_list = cursor.fetchall()
+
+counter = 0
+for xml_link in xml_link_list:
+    # Ná í upplýsingar um öll mál
+    counter += 1
+    data = cache_or_fetch(xml_link[0], force)
+    if counter%20 == 0:
+        print(str(round(counter/len(xml_link_list),2)*100)+"%", end='                \r')  
+
+    # Extract relevant information from the parsed data and insert into the database
+    mal = data['þingmál']['mál']
+    malnumer = mal['@málsnúmer']
+    thingnumer = mal['@þingnúmer']
+    malsheiti = mal['málsheiti']
+    malstegund = mal['málstegund']['heiti']
+    stadamals = ''
+    try:
+        stadamals = mal['staðamáls']
+    except:
+        pass #beiðni um skýrslu sem er fær ekki stöðu
+    framsogumadur = ''
+    try: 
+        framsogumadur = mal['framsogumenn']['framsogumadur']['nafn']
+    except:
+        pass
+    html = mal['slóð']['html']
+    xml = mal['slóð']['xml']
+
+    # Insert or ignore the data into the database
+    cursor.execute('''
+        INSERT OR IGNORE INTO thingmal
+        (malnumer, thingnumer, malsheiti, malstegund, stadamals, framsogumadur, html, xml)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (malnumer, thingnumer, malsheiti, malstegund, stadamals, framsogumadur, html, xml))
+
+    # Commit the changes
+    conn.commit()
+
+#Næ í ræður
+print("Næ i ræður")
+url = "https://www.althingi.is/altext/xml/raedulisti/?lthing=" + current_session
+data = cache_or_fetch(url, force)
+
+# Create table if not exists
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS raedur (
+        id INTEGER PRIMARY KEY,
+        raedumadur TEXT,
+        dagur TEXT,
+        mal_numer INTEGER,
+        mal_heiti TEXT,
+        raeda_hofst TEXT,
+        raeda_lauk TEXT,
+        raeda_texti TEXT,
+        raeda_html TEXT
+    )
+''')
+conn.commit()
+# Iterate through the XML data and insert into the database
+counter = 1
+for raeda in data['ræðulisti']['ræða']:
+    raedumadur = raeda['ræðumaður']['nafn']
+    dagur = raeda['dagur']
+    mal_numer = raeda['mál']['málsnúmer']
+    mal_heiti = raeda['mál']['málsheiti']
+    raeda_hofst = raeda['ræðahófst']
+    raeda_lauk = raeda['ræðulauk']
+    try:
+        raeda_html = raeda['slóðir']['html']
+    except:
+        raeda_html = "" #ræður forseta eru ekki með html slóð
+
+    if 'xml' in raeda['slóðir']:
+        try:
+            raeda_data = cache_or_fetch(raeda['slóðir']['xml'], force)
+            raeda_texti = json.dumps(raeda_data['ræða']['ræðutexti'])
+        except: 
+            print(raeda_texti)
+    else:
+        raeda_texti = ""
+
+    if counter%20 == 0:
+        print(str(round(counter/len(data['ræðulisti']['ræða']),2)*100)+"%", end='                \r')  
+    counter += 1
+
+    # Insert into the database
+    cursor.execute('''
+        INSERT INTO raedur (raedumadur, dagur, mal_numer, mal_heiti, raeda_hofst, raeda_lauk, raeda_texti, raeda_html)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (raedumadur, dagur, mal_numer, mal_heiti, raeda_hofst, raeda_lauk, raeda_texti, raeda_html))
+
+conn.commit()
+conn.close()
