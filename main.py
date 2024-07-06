0eac92fffb6fbd84b95c7292d60fef31c1a1969c
Björn Leví Gunnarsson <bjornlevi@gmail.com>
2024-07-06 00:20:11 +0000

    merge issues with index_template

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import subprocess
-
-def run_script(script_name):
-    try:
-        subprocess.run(["python", script_name], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running {script_name}: {e}")
-
-if __name__ == '__main__':
-    # Run the data fetching script
-    run_script('src/fetch_all_data.py')
-
-    # Run the setup HTML script
-    run_script('src/setup_html.py')
-
-    # Run the generate HTML script
-    run_script('src/generate_html.py')
-
-    print("All scripts executed successfully.")
-=======
 import os
 import subprocess
 import sqlite3
@@ -61,13 +40,17 @@ def main():
         'fetch_malaskra.py',
         'fetch_thingskjol.py',
         'fetch_dagskra.py',
-        'fetch_raedur.py'
+        'fetch_raedur.py',
+        'generate_index_page.py',
+        'generate_thingmenn_page.py'
     ]
 
     for script in scripts:
         print(f"Running {script}...")
-        subprocess.run(['python', os.path.join('scripts', script), force, current_session])
+        try:
+            subprocess.run(['python', os.path.join('scripts', script), force, current_session])
+        except:
+            subprocess.run(['python3', os.path.join('scripts', script), force, current_session])
 
 if __name__ == '__main__':
     main()
->>>>>>> new
